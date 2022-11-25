d3b766a2afe98e66dc66fa588b98c2be25550247
LuiiVasquez <luivasque95@gmail.com>
2022-11-25 17:38:25 -0600

    Error handling

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,62 +1,92 @@
 import Product from "../models/product.model.js";
 
 export const getProduct = async (req, res) => {
-  const {
-    params: { id },
-  } = req;
+  try {
+    const {
+      params: { id },
+    } = req;
 
-  const product = await Product.findById(id);
+    const product = await Product.findById(id);
 
-  if (!product)
-    return res.status(404).json({
-      message: `Product ${id} does not exists`,
+    if (!product)
+      return res.status(404).json({
+        message: `Product ${id} does not exists`,
+      });
+
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
     });
-
-  res.json(product);
+  }
 };
 
 export const getProducts = async (req, res) => {
-  const products = await Product.find();
-
-  res.json(products);
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
 export const createProducts = async (req, res) => {
-  const { name, description, price } = req.body;
+  try {
+    const { name, description, price } = req.body;
 
-  const product = new Product({
-    name,
-    description,
-    price,
-  });
+    const product = new Product({
+      name,
+      description,
+      price,
+    });
 
-  await product.save();
-  res.json(product);
+    await product.save();
+
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
 export const updateProducts = async (req, res) => {
-  const {
-    params: { id },
-  } = req;
+  try {
+    const {
+      params: { id },
+    } = req;
 
-  const productUpdated = await Product.findByIdAndUpdate(id, req.body, {
-    new: true
-  });
+    const productUpdated = await Product.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
 
-  res.json(productUpdated);
+    res.json(productUpdated);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
 export const deleteProducts = async (req, res) => {
-  const {
-    params: { id },
-  } = req;
+  try {
+    const {
+      params: { id },
+    } = req;
 
-  const product = await Product.findByIdAndDelete(id);
+    const product = await Product.findByIdAndDelete(id);
 
-  if (!product)
-    return res.status(404).json({
-      message: `Product ${id} does not exists`,
+    if (!product)
+      return res.status(404).json({
+        message: `Product ${id} does not exists`,
+      });
+
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
     });
-
-  res.json(product);
+  }
 };
