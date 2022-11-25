60240054a85490b12de2cd23a42a9d22dbebaf3c
LuiiVasquez <luivasque95@gmail.com>
2022-11-24 22:32:48 -0600

    setup get and put controllers

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,9 +1,23 @@
-export const getProducts = (req, res) => {
-  res.send("getting produtcs");
+import Product from "../models/product.model.js";
+
+export const getProducts = async (req, res) => {
+  const products = await Product.find();
+
+  res.json(products);
 };
 
-export const createProducts = (req, res) => {
-  res.send("creating produtcs");
+export const createProducts = async (req, res) => {
+  console.log(req.body);
+  const { name, description, price } = req.body;
+
+  const product = new Product({
+    name,
+    description,
+    price,
+  });
+
+  await product.save();
+  res.json(product);
 };
 
 export const updateProducts = (req, res) => {
