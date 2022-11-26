0aa12d4f9d18753d07495bee387a706938d00f94
LuiiVasquez <luivasque95@gmail.com>
2022-11-25 21:52:38 -0600

    reconfiguring environments variables (mongodb_uri)

--- utils/mongoose.js
+++ utils/mongoose.js
@@ -1,13 +1,9 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const MONGO_URI = process.env.MONGO_URI;
+import { MONGODB_URI } from "../config.js";
 
 export async function connectToDB() {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(MONGODB_URI);
     console.log('Mongodb connected');
   } catch (e) {
     console.error(e);
