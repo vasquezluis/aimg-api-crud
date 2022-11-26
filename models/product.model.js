2e6e3fce1081064695e9588fcd75f9b601dbf22c
LuiiVasquez <luivasque95@gmail.com>
2022-11-25 21:45:39 -0600

    adding image model

--- models/product.model.js
+++ models/product.model.js
@@ -16,6 +16,10 @@ const productSchema = mongoose.Schema(
       type: Number,
       default: 0,
     },
+    image: {
+      public_id: String,
+      secure_url: String,
+    },
   },
   {
     timestamps: true,
